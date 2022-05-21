a8680d07dbf9a37d283035ed8a4006155a86f518
Parritz <masong1508@gmail.com>
2022-05-20 21:05:37 -0400

    Move core into init

--- init.lua
+++ init.lua
@@ -1,3 +1,5 @@
-local Core = load("Core")
-
-Core.Initialize()
+local UIInstance = game:GetObjects("rbxassetid://9680652930")[1]
+local UIScreenGui = UIInstance:Clone()
+UIScreenGui.Enabled = false
+UIScreenGui.Parent = gethui and gethui() or game:GetService("CoreGui")
+-- to-do
